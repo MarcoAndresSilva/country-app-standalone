fcf2a6d6f30bd9212e77c2704bcf97e0dcb7225a
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-23 00:27:52 -0400

    feat: add header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatÏconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatÏconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
