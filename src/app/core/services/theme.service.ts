9935d5f3ac887abcc165733059258dbbff9dc6e9
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-24 23:10:49 -0400

    add: config service first step

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export type Theme = 'light-theme' | 'dark-theme'; 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ThemeService {
 
-  constructor() { }
+  private renderer: Renderer2;
+  private _theme = new BehaviorSubject<Theme>(this.getInitialTheme());
+  $theme = this._theme.asObservable();
+
+  constructor( rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  getInitialTheme(): Theme {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme';
+  }
+
 }
+
