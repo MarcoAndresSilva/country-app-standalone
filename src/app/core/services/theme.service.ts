d7e86e5630b4bbdf36dabd2d2ff87362eab84732
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-25 01:03:17 -0400

    add: create functions initializeThem and toogleTheme and setTheme

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -18,17 +18,34 @@ export class ThemeService {
   }
 
   private getInitialTheme(): Theme {
-    // si hay un tema en el almacenamiento local
-    const storeTheme = localStorage.getItem('app-theme') as Theme;
+
+    const storeTheme = localStorage.getItem('app-theme') as Theme;   // si hay un tema en el almacenamiento local
     if(storeTheme) {
       return storeTheme;
     }
-    // si no hay un tema en el almacenamiento local, se utiliza el tema predeterminado
-    if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {    // si no hay un tema en el almacenamiento local, se utiliza el tema predeterminado
       return 'dark-theme';
     }
     return 'light-theme'; // Por defecto
   }
+
+  initializeTheme(): void {
+    this.setTheme().addClass(this._theme.value);
+  }
+
+  toggleTheme(): void {
+    const newTheme = this._theme.value === 'light-theme' ? 'dark-theme' : 'light-theme';
+    this.setTheme(newTheme);
+  }
+
+  private setTheme(theme: Theme ): void {
+    localStorage.setItem('app-theme', theme);
+
+    this._theme.next(theme);
+    this.renderer.addClass(document.body, theme);
+  }
+
+
 }
 
 
