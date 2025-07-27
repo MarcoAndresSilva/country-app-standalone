d5b9bec9107485235dba561e2680a36d6bd86d77
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-26 23:54:28 -0400

    feat: add styles to dark up

--- src/app/features/countries/country-list/country-list.component.ts
+++ src/app/features/countries/country-list/country-list.component.ts
@@ -11,7 +11,7 @@ import { CountryModalComponent } from '../components/country-modal/country-modal
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select'; // Para la Parte 3
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
